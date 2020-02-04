d16fc7bc4790739e9c0c5fcbf916ffb2ad9bda88
Reunji <55166669+Reunji@users.noreply.github.com>
2020-02-04 18:57:25 -0500

    First Checkin

---
+++ GriseldaExample/GriseldaExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriseldaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GriseldaMember BennyTheButcher = new GriseldaMember("BennyTheButcher");
+            GriseldaMember WestSideGunn = new GriseldaMember("West Side Gunn");
+            GriseldaMember ConwayTheMachine = new GriseldaMember("ConwayTheMachine");
+        }
+    }
+}
---
+++ GriseldaExample/GriseldaExample/Rapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriseldaExample
+{
+    class Rapper
+    {
+        public Boolean haveBars;
+        public Boolean canSing;
+        public string Rap()
+        {
+            return "";
+
+            sd
+        }
+    }
+}
